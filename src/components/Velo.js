22202970605b40dee6bd89d95dff912b86660ca6
mmoison22 <moisonmika@gmail.com>
2021-12-15 11:35:28 +0100

    velo

--- src/components/Velo.js
+++ src/components/Velo.js
@@ -2,21 +2,46 @@ import React, { useState } from "react";
 import "./Velo.css";
 
 const Velo = () => {
+    const [model, setModel] = useState(0);
+    const [selle, setSelle] = useState(0);
+
     return (
         <div className="container">
-            <div>
-                Velo
+            <div className="cadreVert">
+                Configure ton vélo
             </div>
             <div>
-                <div>
-
+                <div className="cadreVert">
+                    Choisis ton modèle
                 </div>
-                <div>
-
+                <div onClick={()=>{
+                    setModel(500)
+                }} className="item">
+                    Vélo de ville (500 €)
+                </div>
+                <div onClick={()=>{
+                    setModel(700)
+                }} className="item">
+                    Mountain Bike (700 €)
                 </div>
             </div>
             <div>
-
+                <div className="cadreVert">
+                    Choisis ta selle
+                </div>
+                <div onClick={()=>{
+                    setSelle(20)
+                }} className="item">
+                    Classique (20 €)
+                </div>
+                <div onClick={()=>{
+                    setSelle(80)
+                }} className="item">
+                    Sur suspension (80 €)
+                </div>
+            </div>
+            <div className="cadreVert">
+                Prix total = {model + selle} €
             </div>
         </div>
     );
